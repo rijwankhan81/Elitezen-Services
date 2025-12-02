ac4816a72b181343635341faaa410fcdb32e5751
Rijwan <rijwanjaydee@gmail.com>
2025-12-02 14:44:08 +0530

    download app

---
+++ src/contants/available.tsx
@@ -0,0 +1,29 @@
+import { FaApple, FaGlobe, FaTabletAlt } from "react-icons/fa";
+import { IoLogoAndroid } from "react-icons/io5";
+
+export const available = [
+  {
+    id: 1,
+    icon: <FaApple />,
+    title: "iOS",
+    des: "iPhone & iPad",
+  },
+  {
+    id: 2,
+    icon: <IoLogoAndroid />,
+    title: "Android",
+    des: "All Android devices",
+  },
+  {
+    id: 3,
+    icon: <FaGlobe />,
+    title: "Web App",
+    des: "Any browser",
+  },
+  {
+    id: 4,
+    icon: <FaTabletAlt />,
+    title: "Tablet",
+    des: "Optimized experience",
+  },
+];
