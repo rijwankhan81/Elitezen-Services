023cf5ef4bf25e9c635b6369c97991f8c9042b41
Rijwan <rijwanjaydee@gmail.com>
2026-04-06 16:52:38 +0530

    no need

--- src/constants/companyNav.tsx
+++ src/constants/companyNav.tsx
@@ -1,7 +1,7 @@
 export const companyItems = [
   { label: "About Us", href: "/about" },
   { label: "Compliance Audit", href: "/compliance-audit" },
-  { label: "Careers", href: "/Press Kit" },
+  // { label: "Careers", href: "/Press Kit" },
   { label: "Blog", href: "/blogs" },
   { label: "Contact", href: "/contact" },
 ];
